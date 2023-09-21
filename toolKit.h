020e525deae25a9799ca858492e772ea079e0450
lucasanad9 <lucasanand9@gmail.com>
2023-09-20 22:37:54 -0300

    a biblioteca que estou criando na sua primeira versao, ela contem funcoes que podem ser usadas para vetores, matrizes e outras que ainda estou adicionando

---
+++ toolKit.h
@@ -0,0 +1,19 @@
+// Fun��es para vetores
+void gera_valores( int x[], int k, int limite );
+void gera_valores_ordem( int x[], int k, int limite );
+void mostra_vetor( int x[], int k, char nome[] );
+void mostra_vetor_2( int x[], int k, char nome[] );
+void selection_sort( int x[], int k );
+void bubble_sort( int x[], int k );
+int busca_binaria( int x[], int n, int chave );
+
+// Fun��es para matrizes
+void mostra_matriz( int lin, int col, int x[lin][col] );
+void gera_valores_matriz( int lin, int col, int x[lin][col] );
+void diagonal_principal( int dim, int x[dim][dim] );
+void diagonal_secundaria( int dim, int x[dim][dim] );
+
+//Funçoes matematicas
+int min(int a, int b);
+int max(int a, int b);
+
